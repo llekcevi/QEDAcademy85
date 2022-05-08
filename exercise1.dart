7caf4de6047f7f42750604abbb9fcf4385868095
Luka L <luka.lekcevi@gmail.com>
2022-05-08 13:44:29 +0200

    Solution for first exercise

---
+++ exercise1.dart
@@ -0,0 +1,11 @@
+String firstName = "Luka";
+String lastName = "Lekcevic";
+num age = 27;
+num height = 165;
+
+void main() {
+  print(firstName);
+  print(lastName);
+  print(age);
+  print(height);
+}
