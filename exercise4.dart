9382a0ea8666241ab1583f07dd8387be95948b32
Luka L <luka.lekcevi@gmail.com>
2022-05-08 14:26:40 +0200

    solved fourth exercise with a function

--- exercise4.dart
+++ exercise4.dart
@@ -1,12 +1,16 @@
-int netSalary = 3500;
-int expenses = 3500;
+String calculateBalance(int netSalary, int expenses) {
+  if (netSalary > expenses) {
+    return("You have saved ${netSalary-expenses} this month");
+  } else if (expenses > netSalary) {
+    return("You have lost ${expenses-netSalary} this month");
+  } else {
+    return("Your balance hasn't changed");
+  }
+}
 
 void main () {
-  if (netSalary > expenses) {
-    print("You have saved ${netSalary-expenses} this month");
-  } else if (expenses > netSalary) {
-    print("You have lost ${expenses-netSalary} this month");
-  } else {
-    print("Your balance hasn't changed");
-  }
+  int netSalary = 3500;
+  int expenses = 4500;
+
+  print(calculateBalance(netSalary, expenses));
 }
